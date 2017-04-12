7d6e6ec34aa2c3d6956edd1b20e70e680962eaab
ysCover <1182250786@qq.com>
2017-04-12 10:31:10 +0800

    add Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world!\nhello tc\n");
     }
 } 
